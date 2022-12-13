9c3dbe2f4e7df61baa269cd834e6baebc8efeb0b
Yaroslav9728 <yaroslav9728@gmail.com>
2022-12-12 19:20:08 -0500

    Added screens for help and safety components

--- screens/Help.jsx
+++ screens/Help.jsx
@@ -1,7 +1,87 @@
-import React from 'react'
+import React from "react";
+import {
+  StyleSheet,
+  ImageBackground,
+  SafeAreaView,
+  Text,
+  View,
+  TouchableOpacity,
+} from "react-native";
+import { FlatList } from "react-native-gesture-handler";
+
+const image = require("../assets/help.webp");
+
+const DATA = [
+  { id: 1, header: "Bookings" },
+  { id: 2, header: "Business" },
+  { id: 3, header: "Insurance" },
+  { id: 4, header: "Account and Payments" },
+];
+
+const Card = ({ header }) => (
+  <SafeAreaView>
+    <TouchableOpacity
+      style={styles.text_container}
+      onPress={() => {
+        alert("BACKEND MUST BE IMPLEMENTED FOR THIS FEATURE");
+      }}
+    >
+      <Text style={styles.text}>{header}</Text>
+    </TouchableOpacity>
+  </SafeAreaView>
+);
+const renderCard = ({ item }) => <Card header={item.header} />;
 
 export default function Help() {
   return (
-    <div>Help</div>
-  )
+    <ImageBackground source={image} resizeMode="cover" style={styles.image}>
+      <SafeAreaView style={styles.container}>
+        <Text style={styles.header}>
+          Welcome to ParkVRO Support How can we help?
+        </Text>
+        <FlatList
+          data={DATA}
+          renderItem={renderCard}
+          keyExtractor={(item) => item.id}
+        />
+      </SafeAreaView>
+    </ImageBackground>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+  },
+  header: {
+    marginVertical: 10,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#ffffffc0",
+    padding: 20,
+    textAlign: "center",
+    width: "90%",
+    fontSize: 30,
+    color: "#2f2f2f",
+    fontWeight: "bold",
+  },
+  image: {
+    height: "110%",
+    justifyContent: "center",
+    paddingBottom: 100,
+  },
+  text_container: {
+    padding: 32,
+    margin: 20,
+    backgroundColor: "#ffffffc0",
+    borderRadius: 8,
+  },
+  text: {
+    color: "#2f2f2f",
+    fontSize: 28,
+    lineHeight: 64,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+});
--- screens/Safety.jsx
+++ screens/Safety.jsx
@@ -1,7 +1,39 @@
-import React from 'react'
+import React from "react";
+import { StyleSheet, View, ImageBackground, Text } from "react-native";
+
+const image = require("../assets/safety.jpeg");
 
 export default function Safety() {
   return (
-    <div>Safety</div>
-  )
+    <ImageBackground source={image} resizeMode="cover" style={styles.image}>
+      <View style={styles.container}>
+        <Text style={styles.disclosure}>
+          We want you to move freely, make the most of your time, and be
+          connected to the people and places that matter most to you. That’s why
+          we’re committed to safety—from the creation of new standards to the
+          development of technology with the aim of reducing incidents.
+        </Text>
+      </View>
+    </ImageBackground>
+  );
 }
+
+const styles = StyleSheet.create({
+  image: {
+    height: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  container: {
+    backgroundColor: "#f2f2f2",
+    width: "80%",
+    padding: 22,
+    borderRadius: 20,
+  },
+  disclosure: {
+    fontSize: 23,
+    textAlign: "center",
+    lineHeight: 35,
+    color: "#2f2f2f",
+  },
+});
